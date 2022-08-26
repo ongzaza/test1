de43890bd11a3680ef1746d58a9368ab6c9792c5
Onglortae <ong41544@gmail.com>
2022-08-26 11:15:36 +0700

    Upload web to grid

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import{ Header, Content,Footer} from'./func-components'
+
+function App() {
+  return (
+    [<Header/>,<Content/>,<Footer/>]
+  );
+}
+
+export default App;
